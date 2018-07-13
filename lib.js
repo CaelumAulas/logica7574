550d625c8bf156d9a319925d7f2203cb8f86ee1f
Artur Diniz Adam <artur.daz@gmail.com>
2018-07-13 20:02:31 -0300

    Ficha com acmpanhamaneto

--- lib.js
+++ lib.js
@@ -1,9 +1,3 @@
-function mostra(frase){
-    document.write(frase)
-    document.write("<br><br>")
-    return undefined
-}
-
 function formataMoeda(numero) {
     return "R$ " + numero.toFixed(2).replace(".", ",")
  }
